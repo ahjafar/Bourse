859d3ffbd7cdd650eaf9c5a35156a6dbb97b83e3
AHJafar <amir138106@gmail.com>
2020-08-01 21:29:29 +0430

    added sell model

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Buy
+from .models import Buy,Sell
 
 
 admin.site.register(Buy)
+admin.site.register(Sell)
--- web/models.py
+++ web/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from jdatetime import date,datetime
 
 class Buy(models.Model):
     def __str__(self):
-        return self.name
+        return "{},{}".format(self.name,self.price)
     name=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
     price=models.BigIntegerField()
-    date=models.DateTimeField()
+    year=models.IntegerField(default=date.today().year)
+    month=models.CharField(max_length=40,default=date.today().strftime("%B"))
+    day=models.IntegerField(default=date.today().day)
+
+class Sell(models.Model):
+    def __str__(self):
+        return "{},{}".format(self.name,self.price)
+    name=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    price=models.BigIntegerField()
+    year=models.IntegerField(default=date.today().year)
+    month=models.CharField(max_length=40,default=date.today().strftime("%B"))
+    day=models.IntegerField(default=date.today().day)
